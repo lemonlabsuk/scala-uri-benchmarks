83fee1767cb943973c8a8915849c27674b514a1e
Ian Forsey <forsey@gmail.com>
2020-12-03 22:50:39 +0000

    More IPv6 benchmarks

--- src/main/scala/io/lemonlabs/uri/ParseIpV6Benchmark.scala
+++ src/main/scala/io/lemonlabs/uri/ParseIpV6Benchmark.scala
@@ -1,32 +1,10 @@
 package io.lemonlabs.uri
 
-import io.lemonlabs.uri.ParseIpV6Benchmark.ParseIpV6BenchmarkState
 import org.openjdk.jmh.annotations._
 import org.openjdk.jmh.infra.BenchmarkParams
 
 import java.util.concurrent.TimeUnit
 import scala.util.Random
-
-object ParseIpV6Benchmark {
-
-  @State(Scope.Thread)
-  class ParseIpV6BenchmarkState {
-
-    var regularIpV6: String = _
-
-    @Setup def setUp(params: BenchmarkParams): Unit = {
-
-      def isHex(ch: Char) =
-        (ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'F') || (ch >= 'a' && ch <= 'f')
-
-      val pieceLength = params.getParam("pieceLength").toInt
-
-      def randomPiece = Random.alphanumeric.filter(isHex).take(pieceLength).mkString
-
-      regularIpV6 = List.fill(8)(randomPiece).mkString("[", ":", "]")
-    }
-  }
-}
 
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.AverageTime))
@@ -34,14 +12,52 @@ object ParseIpV6Benchmark {
 @Warmup(iterations = 50, time = 200, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 50, time = 200, timeUnit = TimeUnit.MILLISECONDS)
 class ParseIpV6Benchmark {
-  
-  import org.openjdk.jmh.annotations.Param
 
   @Param(Array("1", "2", "3", "4"))
   private var pieceLength: Int = _
 
+  var regularIpV6: String = _
+  var leadingCompress: String = _
+  var trailingCompress: String = _
+  var middleCompress: String = _
+  var embeddedIpv4: String = _
+
+  @Setup def setUp(): Unit = {
+    def isHex(ch: Char) =
+      (ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'F') || (ch >= 'a' && ch <= 'f')
+
+    def randomPiece = Random.alphanumeric.filter(isHex).take(pieceLength).mkString
+    def randomOctet = Random.nextInt(256)
+
+    regularIpV6 = List.fill(8)(randomPiece).mkString("[", ":", "]")
+    leadingCompress = s"[::$randomPiece]"
+    trailingCompress = s"[$randomPiece::]"
+    middleCompress = s"[$randomPiece:$randomPiece::$randomPiece:$randomPiece]"
+    embeddedIpv4 = "[" + List.fill(6)(randomPiece).mkString(":") + s":$randomOctet.$randomOctet.$randomOctet.$randomOctet]"
+  }
+
   @Benchmark
   @Fork(1)
-  def regularIpV6(s: ParseIpV6BenchmarkState): Unit =
-    IpV6.parse(s.regularIpV6)
+  def parseRegularIpV6(): Unit =
+    IpV6.parse(regularIpV6)
+
+  @Benchmark
+  @Fork(1)
+  def parseLeadingCompress(): Unit =
+    IpV6.parse(leadingCompress)
+
+  @Benchmark
+  @Fork(1)
+  def parseTrailingCompress(): Unit =
+    IpV6.parse(trailingCompress)
+
+  @Benchmark
+  @Fork(1)
+  def parseMiddleCompress(): Unit =
+    IpV6.parse(middleCompress)
+
+  @Benchmark
+  @Fork(1)
+  def parseEmbeddedIpv4(): Unit =
+    IpV6.parse(embeddedIpv4)
 }
